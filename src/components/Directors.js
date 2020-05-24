b123d5a557b0060e3579ac5e023ef73bb6ee1c73
AJ Guiles <ashiguiles@gmail.com>
2020-05-24 20:33:22 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,6 +4,7 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
+     <h1>Directors Page</h1>
       {directors.map((director, index) => (
           <div key={index}>
               <h3>Name: {director.name}</h3>
