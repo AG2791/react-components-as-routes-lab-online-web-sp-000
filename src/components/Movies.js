1897714eda15e0db555c8437b2b6fd5b6a521022
AJ Guiles <ashiguiles@gmail.com>
2020-05-24 20:27:26 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,14 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h3>Name: {movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <p>Genres:</p>
+              <ul>
+                  {movie.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                  ))}
+           </ul>
     </div>
   );
 };
