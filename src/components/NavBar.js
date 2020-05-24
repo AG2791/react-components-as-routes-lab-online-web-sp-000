9e27085d8bafe61f4db7b800291075c3e8747e33
AJ Guiles <ashiguiles@gmail.com>
2020-05-24 20:38:06 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    
   <div className="navbar">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/movies">Movies</NavLink>
